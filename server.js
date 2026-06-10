d387a53f8935ac06bb76a087158e135bdfbf4070
rajunayak555yqy-hash <rajunayak555yqy@gmail.com>
2026-06-10 16:03:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 console.log(process.env.MONGODB_URI);
+console.log('JWT_SECRET:', process.env.JWT_SECRET);
 
 const express = require('express');
 const mongoose = require('mongoose');
